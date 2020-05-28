9f77128b4c193292d039be5764545dbda24b06f8
yuriybk <email2yurii@gmail.com>
2020-05-28 12:53:29 +0300

    project skeleton initialization

---
+++ src/test/java/com/yuriib/springboot/auth/google/springboot_auth_google/SpringbootAuthGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuriib.springboot.auth.google.springboot_auth_google;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAuthGoogleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
